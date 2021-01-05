0ee4c00909bddcf660d928355d4f7a7c4b3b75d9
Matt <matt@GBBAL13096.local>
2021-01-05 22:11:14 +0000

    Clean up

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
 
-  $('.item').matchHeight();
-
-  $(".owl-carousel").owlCarousel({
-    items: 1,
-    loop: true,
-    margin:10,
-    autoHeight:true
-  });
-
 
 });
